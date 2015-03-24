aad8a61d372a08620a2e8f78c8e4b524497b5677
r-a-y <r-a-y@gmx.com>
2015-03-24 15:29:39 -0700

    Filter category and tag lists to use the current BuddyPress page.
    When on a single event page within BuddyPress, the term links default to
    using Event Organiser's archive pages.
    
    This commit filters the term links to reference the current BP context so
    events can be filtered and shown within BP's event calendar.
    
    See #6.

--- includes/functions.php
+++ includes/functions.php
@@ -16,3 +16,49 @@ function bpeo_get_events_slug() {
 function bpeo_get_events_new_slug() {
 	return sanitize_title( constant( 'BPEO_EVENTS_NEW_SLUG' ) );
 }
+
+/** HOOKS ***************************************************************/
+
+/**
+ * Filter event taxonomy term links to match the current BP page.
+ *
+ * BP event content should be displayed within BP instead of event links
+ * linking to Event Organiser's pages.
+ *
+ * @param  string $retval Current term links
+ * @return string
+ */
+function bpeo_filter_term_list( $retval = '' ) {
+	if ( ! is_buddypress() ) {
+		return $retval;
+	}
+
+	global $wp_rewrite;
+
+	$taxonomy = str_replace( 'term_links-', '', current_filter() );
+	$base = str_replace( "%{$taxonomy}%", '', $wp_rewrite->get_extra_permastruct( $taxonomy ) );
+	$base = home_url( $base );
+
+	// group
+	if ( bp_is_group() ) {
+		$bp_base = bpeo_get_group_permalink();
+
+	// assume user
+	} else {
+		$bp_base = trailingslashit( bp_displayed_user_domain() . bpeo_get_events_slug() );
+	}
+
+	// set query arg
+	if ( 'event-tag' === $taxonomy ) {
+		$query_arg = 'tag';
+	} else {
+		$query_arg = 'cat';
+	}
+
+	// string manipulation
+	$retval = str_replace( $base, $bp_base . "?{$query_arg}=", $retval );
+	$retval = str_replace( '/"', '"', $retval );
+	return $retval;
+}
+add_filter( 'term_links-event-tag',      'bpeo_filter_term_list' );
+add_filter( 'term_links-event-category', 'bpeo_filter_term_list' );
