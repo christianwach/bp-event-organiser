14ea0f966a01e95ead15cd884de2b6f9ffd6a8f3
Boone B Gorges <boonebgorges@gmail.com>
2015-03-27 10:24:10 -0400

    Reorganize (or is it reorganise?) JS for better extensibility.

--- assets/js/bp-event-organiser.js
+++ assets/js/bp-event-organiser.js
@@ -1,35 +1,44 @@
-/** 
- * @description: define what happens when the page is ready
- */
-jQuery(document).ready( function($) {
-	var group_id, user_id;
+// Ensure the global `wp` object exists.
+window.wp = window.wp || {};
 
-	// test for our localisation object
-	if ( 'undefined' !== typeof BpEventOrganiserSettings ) {
-		// get our var
-		group_id = BpEventOrganiserSettings.group_id;
-		user_id = BpEventOrganiserSettings.displayed_user_id;
+( function( $ ) {
+	/**
+	 * Bindings etc that happen on $(document).ready.
+	 */
+	init = function() {
+		var group_id, user_id;
+
+		// test for our localisation object
+		if ( 'undefined' !== typeof BpEventOrganiserSettings ) {
+			// get our var
+			group_id = BpEventOrganiserSettings.group_id;
+			user_id = BpEventOrganiserSettings.displayed_user_id;
+		}
+
+		// test if we have wp.hooks
+		if ( 'undefined' !== typeof wp && 'undefined' !== typeof wp.hooks ) {
+
+			// add filter for AJAX request so that the group ID is passed
+			wp.hooks.addFilter(
+				'eventorganiser.fullcalendar_request',
+				function( request, a, b, c, d ) {
+					// add our variable to the request
+					if ( user_id > 0 ) {
+						request['bp_displayed_user_id'] = user_id;
+					}
+
+					if ( group_id > 0 ) {
+						request['bp_group_id'] = group_id;
+					}
+
+					// --<
+					return request;
+				}
+			);
+		}
 	}
 
-	// test if we have wp.hooks
-	if ( 'undefined' !== typeof wp && 'undefined' !== typeof wp.hooks ) {
-
-		// add filter for AJAX request so that the group ID is passed
-		wp.hooks.addFilter(
-			'eventorganiser.fullcalendar_request', 
-			function( request, a, b, c, d ) {
-				// add our variable to the request
-				if ( user_id > 0 ) {
-					request['bp_displayed_user_id'] = user_id;
-				}
-
-				if ( group_id > 0 ) {
-					request['bp_group_id'] = group_id;
-				}
-			
-				// --<
-				return request;
-			}
-		);
-	}
-});
+	$( document ).ready( function() {
+		init();
+	});
+}( jQuery ));
