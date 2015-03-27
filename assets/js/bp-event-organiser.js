342dd2d054b00daa369336189d583e0a42dba4fa
Boone B Gorges <boonebgorges@gmail.com>
2015-03-27 09:32:18 -0400

    More conservative setting of user_id and group_id on calendar AJAX requests.
    There's no need to send 0 values for group_id or user_id.

--- assets/js/bp-event-organiser.js
+++ assets/js/bp-event-organiser.js
@@ -1,52 +1,35 @@
-// init var
-var bp_event_organiser_group_id = 0,
-    bp_event_organiser_displayed_user_id = 0;
-
-// test for our localisation object
-if ( 'undefined' !== typeof BpEventOrganiserSettings ) {
-
-	// get our var
-	bp_event_organiser_group_id = BpEventOrganiserSettings.group_id;
-	bp_event_organiser_displayed_user_id = BpEventOrganiserSettings.displayed_user_id;
-	
-}
-
-
-
 /** 
  * @description: define what happens when the page is ready
  */
 jQuery(document).ready( function($) {
+	var group_id, user_id;
+
+	// test for our localisation object
+	if ( 'undefined' !== typeof BpEventOrganiserSettings ) {
+		// get our var
+		group_id = BpEventOrganiserSettings.group_id;
+		user_id = BpEventOrganiserSettings.displayed_user_id;
+	}
 
 	// test if we have wp.hooks
 	if ( 'undefined' !== typeof wp && 'undefined' !== typeof wp.hooks ) {
 
 		// add filter for AJAX request so that the group ID is passed
 		wp.hooks.addFilter(
-	
 			'eventorganiser.fullcalendar_request', 
 			function( request, a, b, c, d ) {
 				// add our variable to the request
-				request['bp_displayed_user_id'] = bp_event_organiser_displayed_user_id;
-				request['bp_group_id'] = bp_event_organiser_group_id;
-			
-				/*
-				// trace
-				console.log( 'HERE' );
-				console.log( request );
-				console.log( a );
-				console.log( b );
-				console.log( c );
-				console.log( d );
-				*/
+				if ( user_id > 0 ) {
+					request['bp_displayed_user_id'] = user_id;
+				}
+
+				if ( group_id > 0 ) {
+					request['bp_group_id'] = group_id;
+				}
 			
 				// --<
 				return request;
-		
 			}
-		
 		);
-	
 	}
-	
 });
