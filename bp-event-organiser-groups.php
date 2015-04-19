cfbd12d9531e43d6689f530341f28b91ac224c2e
r-a-y <r-a-y@gmx.com>
2015-04-19 10:53:31 -0700

    Groups: Show featured image on a single event page.
    Size is hardcoded to 'medium' size at the moment.
    
    See #17.

--- bp-event-organiser-groups.php
+++ bp-event-organiser-groups.php
@@ -344,6 +344,9 @@ class BP_Event_Organiser_Group_Extension extends BP_Group_Extension {
 		// Make this better... have to juggle the_content filters...
 		echo wpautop( $post->post_content );
 
+		// post thumbnail - hardcoded to medium size at the moment.
+		the_post_thumbnail( 'medium' );
+
 		add_action( 'loop_end', array( $this, 'catch_reset_postdata' ) );
 		eo_get_template_part( 'event-meta-event-single' );
 		remove_action( 'loop_end', array( $this, 'catch_reset_postdata' ) );
