3a74d1e25627d50c9f577a3052000ed424a45bfc
r-a-y <r-a-y@gmx.com>
2015-04-23 10:59:13 -0700

    Format content-event.php template part.

--- templates/content-event.php
+++ templates/content-event.php
@@ -1,13 +1,10 @@
 
 	<h4><?php _e( 'Event Description', 'bp-event-organizer' ); ?></h4>
 
-	<?php
-	the_content();
+	<?php the_content(); ?>
 
-	// post thumbnail - hardcoded to medium size at the moment.
-	the_post_thumbnail( 'medium' );
+	<?php the_post_thumbnail( 'medium' ); ?>
 
-	eo_get_template_part( 'event-meta', 'event-single' );
+	<?php eo_get_template_part( 'event-meta', 'event-single' ); ?>
 
-	bpeo_the_single_event_action_links();
-	?>
+	<?php bpeo_the_single_event_action_links(); ?>
