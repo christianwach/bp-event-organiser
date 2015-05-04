cc5b375406aa6b0d2946db313db2ac2ea786fdbd
r-a-y <r-a-y@gmx.com>
2015-05-04 16:22:37 -0700

    Second pass at toggling visibility when a group is added to an event.
    This commit now adds a message in the 'Publish' metabox when a group is
    added.  The 'Status' and 'Visibility' fields are now hidden when a group is
    added to prevent the user from modifying these options.
    
    See #28.

--- bp-event-organiser-eo.php
+++ bp-event-organiser-eo.php
@@ -279,7 +279,7 @@ class BuddyPress_Event_Organiser_EO {
 			<?php
 				foreach( $this->group_ids as $gid ) {
 					$group = groups_get_group( array( 'group_id' => $gid ) );
-					$private = 'public' !== $group->status ? 'data-private="1"' : '';
+					$private = 'public' !== $group->status ? 'title="Private"' : '';
 					echo "<option value='{$gid}' selected='selected' {$private}>{$group->name}</option>";
 				}
 			?>
@@ -290,93 +290,122 @@ class BuddyPress_Event_Organiser_EO {
 		<?php endif; ?>
 
 		<script type="text/javascript">
-			var bpeoPrivateFlag = 0,
-				bpeoToggleFlag = false;
+			var bpeoGroupMsg = '<?php echo esc_js( __( 'You have added a group to this event.  Since groups have their own privacy settings, we have removed the ability to set the status for this event.', 'bp-event-organizer' ) ); ?>',
+				bpeoToggleFlag = false,
+				bpeoCurrStatus = '';
 
-			bpeoSelect = jQuery('#bp_event_organiser_metabox select');
-			bpeoPrivateFlag = bpeoSelect.find('[data-private]').length;
+			jQuery(function($){
+				bpeoCurrStatus = $('#post-status-display').text();
+				bpeoCurrVisibility = $('#post-visibility-display').text();
+				bpeoSelect = $('#bp_event_organiser_metabox select');
+				bpeoPrivateFlag = bpeoSelect.find('[title]').length;
+				bpeoSubmit = $('#submitdiv .inside');
 
-			if ( bpeoPrivateFlag ) {
-				bpeoToggleFlag = true;
-			}
+				bpeoToggle = function() {
+					var notice = bpeoSubmit.find('.updated');
 
-			bpeoToggle = function() {
-				if ( bpeoPrivateFlag === 1 ) {
-					bpeoToggleFlag = true;
-					jQuery("#visibility-radio-private").prop("checked", true);
-					jQuery('#visibility a').hide();
-					jQuery('#post-visibility-display').fadeOut('fast').text( postL10n.private ).fadeIn('fast');
+					if ( false === $.isEmptyObject( bpeoSelect.val() ) ) {
+						bpeoToggleFlag = true;
 
-				} else if ( bpeoPrivateFlag === 0 && bpeoToggleFlag === true ) {
-					bpeoToggleFlag = false;
-					jQuery("#visibility-radio-public").prop("checked", true);
-					jQuery('#visibility a').show();
-					jQuery('#post-visibility-display').fadeOut('fast').text( postL10n.public ).fadeIn('fast');
+						if ( bpeoPrivateFlag === 1 ) {
+							$("#visibility-radio-private").prop("checked", true);
+							$('#post-status-display').fadeOut('fast').text( postL10n.privatelyPublished ).fadeIn('fast');
+							$('#post-visibility-display').fadeOut('fast').text( postL10n.private ).fadeIn('fast');
+						}
+
+						$('.misc-pub-post-status, .misc-pub-visibility').hide();
+						$('#save-post').hide();
+						$('#submitdiv .inside .error').show();
+
+						if ( ! notice.length && typeof adminpage === 'undefined' ) {
+							bpeoSubmit.prepend('<div class="updated"><p>' + bpeoGroupMsg + '</p></div>');
+						} else {
+							notice.fadeIn('fast');
+						}
+
+					} else if ( bpeoPrivateFlag === 0 && bpeoToggleFlag === true ) {
+						bpeoToggleFlag = false;
+						$("#visibility-radio-public").prop("checked", true);
+						$('.misc-pub-post-status, .misc-pub-visibility').show();
+						$('#post-status-display').fadeOut('fast').text( bpeoCurrStatus ).fadeIn('fast');
+						$('.edit-post-status').show();
+						$('#post-visibility-display').fadeOut('fast').text( bpeoCurrVisibility ).fadeIn('fast');
+						$('#save-post').show();
+						$('#submitdiv .inside .error').hide();
+
+						if ( notice.length ) {
+							notice.fadeOut('fast');
+						}
+					}
 				}
-			}
 
-			bpeoFormatResponse = function(data) {
-				return data.name || data.text;
-			}
+				bpeoToggle();
 
-			bpeoFormatResult = function(data) {
-				if (data.loading) return data.name;
-
-				var markup = '<div style="clear:both;">' +
-				'<div style="float:left;margin-right:8px;">' + data.avatar + '</div>' +
-				'<div><span style="font-weight:600;">' + data.name + '</span> <em>(' + data.type + ')</em></div>';
-
-				if (data.description) {
-					markup += '<div style="font-size:.9em;line-height:1.9;">' + data.description + '</div>';
+				bpeoFormatResponse = function(data) {
+					return data.name || data.text;
 				}
 
-				markup += '</div>';
+				bpeoFormatResult = function(data) {
+					if (data.loading) return data.name;
 
-				return markup;
-			}
+					var markup = '<div style="clear:both;">' +
+					'<div style="float:left;margin-right:8px;">' + data.avatar + '</div>' +
+					'<div><span style="font-weight:600;">' + data.name + '</span> <em>(' + data.type + ')</em></div>';
 
-			bpeoSelect.select2({
-				ajax: {
-					method: 'POST',
-					url: ajaxurl,
-					dataType: 'json',
-					delay: 500,
-					data: function (params) {
-						return {
-							s: params.term, // search term
-							action: 'bpeo_get_groups',
-							nonce: jQuery('#bp_event_organiser_nonce_field').val(),
-							page: params.page
-						};
+					if (data.description) {
+						markup += '<div style="font-size:.9em;line-height:1.9;">' + data.description + '</div>';
+					}
+
+					markup += '</div>';
+
+					return markup;
+				}
+
+				bpeoSelect.select2({
+					ajax: {
+						method: 'POST',
+						url: ajaxurl,
+						dataType: 'json',
+						delay: 500,
+						data: function (params) {
+							return {
+								s: params.term, // search term
+								action: 'bpeo_get_groups',
+								nonce: $('#bp_event_organiser_nonce_field').val(),
+								page: params.page
+							};
+						},
+						cache: true,
+						processResults: function (data, page) {
+							// parse the results into the format expected by Select2.
+							// since we are using custom formatting functions we do not need to
+							// alter the remote JSON data
+							return {
+								results: data
+							};
+						},
 					},
-					cache: true,
-					processResults: function (data, page) {
-						// parse the results into the format expected by Select2.
-						// since we are using custom formatting functions we do not need to
-						// alter the remote JSON data
-						return {
-							results: data
-						};
-					},
-				},
-				escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
-				minimumInputLength: 3,
-				templateResult: bpeoFormatResult,
-				templateSelection: bpeoFormatResponse
-			});
+					escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+					minimumInputLength: 3,
+					templateResult: bpeoFormatResult,
+					templateSelection: bpeoFormatResponse
+				});
 
-			bpeoSelect.on("select2:unselecting", function (e) {
-				if ( 'data-private' === e.currentTarget.lastElementChild.attributes[0].name || true === e.params.args.data.private ) {
-					bpeoPrivateFlag--;
-					bpeoToggle();
-				}
-			});
+				bpeoSelect.on("select2:unselecting", function (e) {
+					if ( 'Private' == e.params.args.data.title ) {
+						bpeoPrivateFlag--;
+					}
+				});
 
-			bpeoSelect.on("select2:selecting", function (e) {
-				if ( true === e.params.args.data.private ) {
-					bpeoPrivateFlag++;
+				bpeoSelect.on("select2:selecting", function (e) {
+					if ( 'Private' == e.params.args.data.title ) {
+						bpeoPrivateFlag++;
+					}
+				});
+
+				bpeoSelect.on("select2:unselect select2:select", function (e) {
 					bpeoToggle();
-				}
+				});
 			});
 		</script>
 	<?php
