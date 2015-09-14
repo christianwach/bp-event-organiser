47e26a7fa8a783adbcc12bb252e28fed7d808f6b
r-a-y <r-a-y@gmx.com>
2015-09-14 12:43:04 -0700

    Select2: Only register script if it hasn't already been registered.
    This is to avoid conflicts with other plugins using Select2 such as
    Shortcake.
    
    We're also changing our enqueue handle from 'bpeo-select2' to 'select2' to
    be more universal.

--- includes/functions.php
+++ includes/functions.php
@@ -21,12 +21,15 @@ function bpeo_get_events_new_slug() {
  * Register common assets.
  */
 function bpeo_register_assets() {
-	// Styles.
-	wp_register_style( 'bpeo-select2', set_url_scheme( 'http://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/css/select2.min.css' ) );
+	// Select2
+	if ( false === wp_script_is( 'select2' ) ) {
+		wp_register_script( 'select2', set_url_scheme( 'http://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/js/select2.min.js', array( 'jquery' ) ) );
+		wp_register_style( 'select2', set_url_scheme( 'http://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/css/select2.min.css' ) );
+	}
 
 	wp_register_script( 'bp_event_organiser_js', BUDDYPRESS_EVENT_ORGANISER_URL . 'assets/js/bp-event-organiser.js', array( 'jquery' ), BUDDYPRESS_EVENT_ORGANISER_VERSION, true );
-	wp_register_script( 'bpeo-select2', set_url_scheme( 'http://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/js/select2.min.js', array( 'jquery' ) ) );
-	wp_register_script( 'bpeo-group-select', BUDDYPRESS_EVENT_ORGANISER_URL . 'assets/js/group-select.js', array( 'jquery', 'bpeo-select2' ), BUDDYPRESS_EVENT_ORGANISER_VERSION, true );
+
+	wp_register_script( 'bpeo-group-select', BUDDYPRESS_EVENT_ORGANISER_URL . 'assets/js/group-select.js', array( 'jquery', 'select2' ), BUDDYPRESS_EVENT_ORGANISER_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts',    'bpeo_register_assets', 5 );
 add_action( 'admin_enqueue_scripts', 'bpeo_register_assets', 5 );
